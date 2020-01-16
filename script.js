eaa3ef4c100efb6c960bc32f647fe20b1f2a63b1
clair13 <theclairvoyant1313@yahoo.gr>
2020-01-16 19:44:41 +0200

    Create addBookToList method

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
-function Book(title, author, pages, read = true) {
-  this.title = title
-  this.author = author
-  this.pages = pages
-  this.read = read ? "yes" : "no"
+class Book {
+  constructor(title, author, pages, read = true) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages.author;
+    this.read = read ? "yes" : "no";
+  }
+ }
+
+
+//UI
+class UI {
+  static displayBooks() {
+    const StoredBooks = [
+      {
+        title: "Book One",
+        author: "Author One",
+        pages: "101",
+        read: "yes"
+      },
+      {
+        title: "Book Two",
+        author: "Author Two",
+        pages: "202",
+        read: "yes"
+      }
+    ];
+
+    const books =StoredBooks;
+
+    books.forEach((book) => UI.addBookToList(book));
+  }
 }
