fb6889f09e8caa8a44d82c2b9dfdbeff9cd94e05
clair13 <theclairvoyant1313@yahoo.gr>
2020-01-16 22:36:49 +0200

    Get values from the form

--- script.js
+++ script.js
@@ -30,4 +30,36 @@ class UI {
 
     books.forEach((book) => UI.addBookToList(book));
   }
+
+  static addBookToList(book) {
+    const list = document.querySelector('#book-list');
+
+    const row = document.createElement('tr');
+
+    row.innerHTML = `
+      <td>${book.title}</td>
+      <td>${book.author}</td>
+      <td>${book.pages}</td>
+      <td>${book.read}</td>
+      <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+      `;
+
+    list.appendChild(row);
+  }
 }
+
+// Event: display Books
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
+
+// Event: Add a Book
+document.querySelector('#book-form').addEventListener('submit',(e) => {
+  e.preventDefault();
+
+  //Get form values
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  const pages = document.querySelector('#pages').value;
+  const read = document.querySelector('#read').value;
+
+  const book = new Book(title,author,pages,read);
+});
