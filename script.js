1510fca4c7b5da012ff19c7661e03be726559841
clair13 <theclairvoyant1313@yahoo.gr>
2020-01-19 19:53:22 +0200

    Implement addBook method for localStorage

--- script.js
+++ script.js
@@ -68,6 +68,12 @@ class Store {
     }
     return books;
   }
+
+  static addBook(book) {
+    const books = Store.getBooks();
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
+  }
 }
 
 // Event: display Books
